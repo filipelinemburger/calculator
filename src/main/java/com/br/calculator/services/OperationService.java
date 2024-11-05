56a303a9e0a63f0a6a51b86192d345346904fe56
Filipe Linemburger <filipelinemburger@gmail.com>
2024-11-05 11:36:17 -0300

    Remove unnecessary parameters

--- src/main/java/com/br/calculator/services/OperationService.java
+++ src/main/java/com/br/calculator/services/OperationService.java
@@ -38,22 +38,20 @@ public class OperationService {
 
     private static final Logger logger = LoggerFactory.getLogger(OperationService.class);
 
-    private static final int INITIAL_AMOUNT = 200;
+
     @Value("${aws.lambda.function}")
     private String LAMBDA_FUNCTION;
 
     private final CacheManager cacheManager;
-    private final RecordService recordService;
     private final OperationRepository operationRepository;
     private final RecordRepository recordRepository;
     private final LambdaClient lambdaClient;
 
-    public OperationService(RecordService recordService, OperationRepository operationRepository, RecordRepository recordRepository, CacheManager cacheManager) {
+    public OperationService(OperationRepository operationRepository, RecordRepository recordRepository, CacheManager cacheManager) {
         this.lambdaClient = LambdaClient.builder()
                 .region(Region.US_EAST_2)
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
-        this.recordService = recordService;
         this.operationRepository = operationRepository;
         this.recordRepository = recordRepository;
         this.cacheManager = cacheManager;
